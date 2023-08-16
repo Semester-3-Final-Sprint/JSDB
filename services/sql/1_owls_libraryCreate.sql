557baa1a34b9b949b01667b3fef43b6431f0e60e
Jonah Greening <116300133+JonahJG@users.noreply.github.com>
2023-08-16 15:17:14 -0230

    updated sql statement names

---
+++ services/sql/1_owls_libraryCreate.sql
@@ -0,0 +1,13 @@
+-- Database: owls_library
+
+-- DROP DATABASE IF EXISTS owls_library;
+
+CREATE DATABASE owls_library
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_Canada.1252'
+    LC_CTYPE = 'English_Canada.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
